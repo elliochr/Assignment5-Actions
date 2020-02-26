63d0b97db36eafe2b5c3900063e277ff53a6af46
elliochr <46430710+elliochr@users.noreply.github.com>
2020-02-25 19:34:29 -0800

    added task.py and tests.py

---
+++ tests.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import unittest
+import task
+
+class TestCase(unittest.TestCase):
+
+    def test1(self):
+        expected = "success"
+        self.assertEqual(expected, task.firstrun())
+
+
+    def test2(self):
+        expected = "failure"
+        self.assertNotEqual(expected, task.firstrun())
+
+
+if __name__ == '__main__':
+    unittest.main()
+
