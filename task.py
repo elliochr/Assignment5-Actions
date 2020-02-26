1446f36a5a5a461935d71a0054f9749735ecf7f9
elliochr <46430710+elliochr@users.noreply.github.com>
2020-02-25 20:29:15 -0800

    first last list

--- task.py
+++ task.py
@@ -7,3 +7,7 @@ def firstrun():
 
 def circleArea(radius):
     return radius * radius * 3.14159
+
+
+def firstLast(myList):
+    return [myList[0], myList[-1]]
--- tests.py
+++ tests.py
@@ -18,6 +18,10 @@ class TestCase(unittest.TestCase):
         expected = 28.27431
         self.assertAlmostEqual(expected, task.circleArea(3), None, None, 0.01)
 
+    def testFirstLast(self):
+        expected = [1, 9]
+        self.assertListEqual(expected, task.firstLast([1,2,3,4,5,6,7,8,9]))
+
 
 if __name__ == '__main__':
     unittest.main()
